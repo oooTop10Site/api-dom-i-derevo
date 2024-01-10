546283657512e610627378f99a5c32edc70d30cd
Fedorov Nikita <fedorov.nukuta@gmail.com>
2024-01-10 11:21:17 +0300

    Add logout link

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="{{ route('auth.logout') }}">
                 <i class="fas fa-sign-out-alt"></i>
             </a>
         </li>
